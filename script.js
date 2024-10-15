91504f8ffb04a0962b6e2cf491c3e39cf89c12b7
MoonLightCodes <poornachander295@gmail.com>
2024-10-16 02:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+function handleS() {
+	const ht = document.getElementById("url");
+	const nm = document.getElementById("name").value;
+	const yr = document.getElementById("year").value;
+	let s= "https://localhost:8080/?";
+	if(nm){
+		s+=`name=${nm}`;
+	}
+	if(yr){
+		if(nm){
+			s+='&';
+		}
+		s+=`year=${yr}`
+	}
+	ht.textContent = s;
+}
